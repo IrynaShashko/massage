4e49afa8ec7ca3c25d637da19dae1d9fe47c6635
IrynaShashko <irishkashashko@gmail.com>
2023-04-25 20:52:40 +0300

    add grid to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,40 @@
+import "../../pages/HomePage/HomePage.css";
+import photo1 from "../../images/10.jpg";
+import photo2 from "../../images/11.jpg";
+import photo3 from "../../images/13.jpg";
+
 const HomePage = () => {
   return (
     <main>
       <div>
-        <h1>
-          Я знаю все про <span>ЯКІСНИЙ</span> масаж
-        </h1>
+        <div class="container">
+          <div class="photo1">
+            <img
+              className="img1"
+              src={photo1}
+              width={100}
+              height="100%"
+              alt="logo"
+            />
+          </div>
+          <div class="photo2">
+            <img src={photo2} width={100} height="100%" alt="logo" />
+          </div>
+          <div class="photo3">
+            <img
+              className="img3"
+              src={photo3}
+              width={100}
+              height="100%"
+              alt="logo"
+            />
+          </div>
+          <div class="text">
+            <p>
+              Я знаю все про <span className="span">ЯКІСНИЙ</span> масаж
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
