e4b4f198d4ee7ddc1fcd2c6ebd0a8a15b89561fe
IrynaShashko <irishkashashko@gmail.com>
2023-04-25 11:17:46 +0300

    Updates

---
+++ static/js/680.66fe513a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmassage=self.webpackChunkmassage||[]).push([[680],{680:function(s,e,n){n.r(e);var a=n(184);e.default=function(){return(0,a.jsx)("main",{children:(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["\u042f \u0437\u043d\u0430\u044e \u0432\u0441\u0435 \u043f\u0440\u043e ",(0,a.jsx)("span",{children:"\u042f\u041a\u0406\u0421\u041d\u0418\u0419"})," \u043c\u0430\u0441\u0430\u0436"]})})})}}}]);
+//# sourceMappingURL=680.66fe513a.chunk.js.map
