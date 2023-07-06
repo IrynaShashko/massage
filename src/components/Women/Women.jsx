5f26dd36a12beb7da5f8380f35d4d75b53ed867c
IrynaShashko <irishkashashko@gmail.com>
2023-07-06 17:09:03 +0300

    fix

--- src/components/Women/Women.jsx
+++ src/components/Women/Women.jsx
@@ -1,5 +1,27 @@
-const Women = () => {
-  <ul></ul>;
-};
+import { Link } from "react-router-dom";
 
-export default Women;
+import priceData from "../../../../public/price.json";
+
+export default function Women() {
+  const womenData = priceData.women;
+
+  return (
+    <section>
+      <div>
+        <Link className="link" href="/price">
+          <button>Назад</button>
+        </Link>
+        <ul>
+          <h2>Жіночі послуги</h2>
+          {womenData.map((item) => (
+            <li>
+              <p>{item.service}</p>
+              <p>{item.time}</p>
+              <p>{item.price}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
