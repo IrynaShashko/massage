e393cf0f68a38ad0a1449c5a6b203d678ceaf697
IrynaShashko <irishkashashko@gmail.com>
2023-07-17 22:35:53 +0300

    add AboutPage

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,11 @@
+import photo from "../../images/photo.jpg";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <img src={photo} alt="masseur" width={200} />
+    </div>
+  );
+};
+
+export default AboutPage;
