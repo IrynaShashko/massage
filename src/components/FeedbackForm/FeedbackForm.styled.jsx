2624adc962859e7c4834e9f98592f07ae8ef2da0
IrynaShashko <irishkashashko@gmail.com>
2023-07-17 14:14:57 +0300

    add feedback.styled

---
+++ src/components/FeedbackForm/FeedbackForm.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import { Formik } from "formik";
+
+export const FormContainer = styled(Formik)`
+  width: 600px;
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; */
+  padding: 20px;
+  border-radius: 20px;
+  border: 1px solid #007586;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
