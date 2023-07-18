c5bfad7cbd74952a9561aa3fe5c9b80b94b3bb97
IrynaShashko <irishkashashko@gmail.com>
2023-07-19 02:26:43 +0300

    ашч

--- src/components/ConnectionForm.jsx/СonnectionModal.jsx
+++ src/components/ConnectionForm.jsx/СonnectionModal.jsx
@@ -129,7 +129,7 @@ const ConnectionForm = ({ isOpen, onClose }) => {
           <FormTitle>Записатись</FormTitle>
           <Formik initialValues={initialValues} onSubmit={handleSubmit}>
             {({ values, handleChange }) => (
-              <FormStyled action="/success/success.html" data-netlify="true">
+              <FormStyled data-netlify="true">
                 <div>
                   <Label htmlFor="service">
                     Для кого:
