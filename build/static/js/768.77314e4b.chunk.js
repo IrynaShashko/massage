8fc76c686f1e5a64f084bb96032e2a4a771b4191
IrynaShashko <irishkashashko@gmail.com>
2023-07-19 11:50:45 +0300

    fix

--- build/static/js/768.77314e4b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmassage=self.webpackChunkmassage||[]).push([[768],{5401:function(s,e,a){a.r(e),a.d(e,{default:function(){return u}});var t=a.p+"static/media/photo.14f2e93b48254560a81b.jpg",n=a(184),u=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("img",{src:t,alt:"masseur",width:200})})}}}]);
-//# sourceMappingURL=768.77314e4b.chunk.js.map
